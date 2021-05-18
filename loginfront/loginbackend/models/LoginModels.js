1cda259265fa7e385dc75b6acc361f735a58a58a
ytang913 <ytang18@terpmail.umd.edu>
2021-05-18 14:25:40 -0700

    Update LoginModels.js

---
+++ loginfront/loginbackend/models/LoginModels.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const loginTemplate = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required:true
+    },
+    username:{
+        type: String, 
+        required:true
+    },
+    email: {
+        type: String,
+        required:true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    
+})
