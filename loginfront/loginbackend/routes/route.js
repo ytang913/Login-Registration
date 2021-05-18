07f66505e0c95386295dc97f876f8088381d3414
ytang913 <ytang18@terpmail.umd.edu>
2021-05-18 15:18:03 -0700

    updating

--- loginfront/loginbackend/routes/route.js
+++ loginfront/loginbackend/routes/route.js
@@ -4,7 +4,10 @@ const loginTemplateCopy = require('../models/LoginModels')
 
 router.post('/signup', (request, response) =>{
     const signupUser = new loginTemplateCopy({
-        fullName: request.body.fullName
+        fullName: request.body.fullName,
+        username: request.body.username,
+        email: request.body.email,
+        password: request.body.password
     })
 })
 
