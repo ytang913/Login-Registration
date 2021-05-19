17ebec5b42adcacbcaba784597afe0279b4df510
ytang913 <ytang18@terpmail.umd.edu>
2021-05-19 14:28:19 -0700

    updating

---
+++ loginfront/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App  extends Component {
+    constructor(){
+        super()
+        this.state = {
+            fullName = '',
+            username = '',
+            email = '',
+            password = '',
+
+        }
+    }
+
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default App ;
