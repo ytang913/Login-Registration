bf91f96e45f0d24a57a22db64bdb7e9c443aff06
ytang913 <ytang18@terpmail.umd.edu>
2021-05-18 18:50:31 -0700

    updating

--- loginfront/loginbackend/server.js
+++ loginfront/loginbackend/server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app  = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const routesURL = require('./routes/route')
+const cors = require('cors')
 
 dotenv.config()
 
 mongoose.connect(process.env.DATABASE_ACCESS, () => console.log("Database has been connected"))
 
-
+app.use(express.json()) //body parser
+app.use(cors())
+app.use('./app',routesURL)
 app.listen(4000, () => console.log("server is up and running"))
 
